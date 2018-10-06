9fc3ad319e23fbe5a5fb8308adb4bfba6b20ad3b
mo3in <moein.hente@gmail.com>
2018-10-06 20:38:43 +0330

    some fix

--- src/Kasp.EF/Extensions/AppBuilderExtensions.cs
+++ src/Kasp.EF/Extensions/AppBuilderExtensions.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Kasp.EF.Extensions {
 	public static class AppBuilderExtensions {
 		public static KaspDbAppBuilder UseDataBase<TDbContext>(this KaspAppBuilder builder, bool applyMigrates = true) where TDbContext : DbContext {
-			var db = builder.ApplicationBuilder.ApplicationServices.GetService<TDbContext>();
+			var db = builder.ApplicationBuilder.ApplicationServices.CreateScope().ServiceProvider.GetService<TDbContext>();
 
 			var result = new KaspDbAppBuilder(builder, db);
 
