8b67fc8333aa1b56a017ee8b2e3bd46bc7df9a17
mo3in <moein.hente@gmail.com>
2019-01-20 16:18:21 +0330

    form builder tests init

---
+++ tests/Kasp.FormBuilder.Tests/Startup.cs
@@ -0,0 +1,27 @@
+using Kasp.FormBuilder.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kasp.FormBuilder.Tests {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddFormBuilder();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            app.UseMvc();
+        }
+    }
+}
--- tests/Kasp.FormBuilder.Tests/UnitTest1.cs
+++ tests/Kasp.FormBuilder.Tests/UnitTest1.cs
@@ -1,8 +1,12 @@
-using System;
+using Kasp.Test;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Kasp.FormBuilder.Tests {
-    public class UnitTest1 {
+    public class UnitTest1 : KClassFixtureWebApp<Startup> {
+        public UnitTest1(ITestOutputHelper output, KWebAppFactory<Startup> factory) : base(output, factory) {
+        }
+        
         [Fact]
         public void Test1() {
         }
