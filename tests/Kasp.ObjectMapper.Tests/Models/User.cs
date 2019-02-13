a75d44280c3200af6b2ff2b962abf96b5bdd9df2
Mo3in <moein.hente@gmail.com>
2019-02-13 23:44:55 +0330

    challenge

---
+++ tests/Kasp.ObjectMapper.Tests/Models/User.cs
@@ -0,0 +1,7 @@
+namespace Kasp.ObjectMapper.Tests.Models {
+	public class User {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Family { get; set; }
+	}
+}
---
+++ tests/Kasp.ObjectMapper.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Kasp.ObjectMapper.Extensions;
+using Kasp.ObjectMapper.Tests.Models;
+using Xunit;
+
+namespace Kasp.ObjectMapper.Tests {
+	public class UnitTest1 {
+		[Fact]
+		public void Test1() {
+			var user = new User();
+			var userVm = user.MapTo<UserVm>();
+		}
+	}
+}
