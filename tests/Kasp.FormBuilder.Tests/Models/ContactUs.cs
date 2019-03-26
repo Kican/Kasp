cf60908d2b60767afabc2938d548c28a36d8b664
Mo3in <moein.hente@gmail.com>
2019-03-26 10:54:36 +0430

    :)

---
+++ tests/Kasp.FormBuilder.Tests/Models/ContactUs.cs
@@ -0,0 +1,8 @@
+namespace Kasp.FormBuilder.Tests.Models {
+	public class ContactUs {
+		public string Email { get; set; }
+		public string Name { get; set; }
+		public string PhoneNumber { get; set; }
+		public string Content { get; set; }
+	}
+}
--- tests/Kasp.FormBuilder.Tests/UnitTest1.cs
+++ tests/Kasp.FormBuilder.Tests/UnitTest1.cs
@@ -1,15 +1,21 @@
+using System.Threading.Tasks;
+using Kasp.FormBuilder.Services;
+using Kasp.FormBuilder.Tests.Models;
 using Kasp.Test;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Kasp.FormBuilder.Tests {
 	public class UnitTest1 : KClassFixtureWebApp<Startup> {
-
+		public IFormBuilder FormBuilder { get;}
 		public UnitTest1(ITestOutputHelper output, KWebAppFactory<Startup> factory) : base(output, factory) {
+			FormBuilder = GetService<IFormBuilder>();
 		}
 
 		[Fact]
-		public void Test1() {
+		public async Task Test1() {
+			var form = await FormBuilder.FromModel<ContactUs>();
+//			form.
 			Assert.True(1 == 2 - 1);
 		}
 	}
