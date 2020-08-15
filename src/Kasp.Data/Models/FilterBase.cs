6ce5b92f5f0c3286c7cedd7bfaee565c4a112715
mo3in <moein.hente@gmail.com>
2020-08-15 09:27:47 +0430

    set FilterBase.cs default value

--- src/Kasp.Data/Models/FilterBase.cs
+++ src/Kasp.Data/Models/FilterBase.cs
@@ -1,8 +1,8 @@
 ﻿namespace Kasp.Data.Models {
 	public class FilterBase : IPage, IOrder {
-		public int Page { get; set; }
-		public int Count { get; set; }
-		
+		public int Page { get; set; } = 1;
+		public int Count { get; set; } = 10;
+
 		public string OrderBy { get; set; }
 		public bool IsDesc { get; set; }
 
