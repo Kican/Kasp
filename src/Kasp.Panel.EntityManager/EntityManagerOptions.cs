71dab24399acfe1e94197b5bca398478fe692888
mo3in <moein.hente@gmail.com>
2020-08-18 14:37:57 +0430

    entity-manager route from RouteAttribute

--- src/Kasp.Panel.EntityManager/EntityManagerOptions.cs
+++ src/Kasp.Panel.EntityManager/EntityManagerOptions.cs
@@ -1,19 +1,38 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 using System.Reflection;
+using Kasp.FormBuilder.Extensions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Kasp.Panel.EntityManager {
 	public class EntityManagerOptions {
 		public List<EntityManagerInfo> Managers { get; set; } = new List<EntityManagerInfo>();
 
 		public EntityManagerOptions AddFromAssembly<T>() {
-			return this;
+			return AddFromAssembly(typeof(T).Assembly);
 		}
 
 		public EntityManagerOptions AddFromAssemblies(Assembly[] assemblies) {
+			foreach (var assembly in assemblies) {
+				AddFromAssembly(assembly);
+			}
+
 			return this;
 		}
 
 		public EntityManagerOptions AddFromAssembly(Assembly assembly) {
+			var entityManagerType = typeof(EntityManagerControllerBase<,,,,,,>);
+			var types = assembly.GetTypes()
+				.Where(x => !x.IsAbstract && x.GetCustomAttribute<NotMappedAttribute>() == null && entityManagerType.IsAssignableFrom(x))
+				.ToArray();
+
+			foreach (var type in types) {
+				var route = type.GetCustomAttribute<RouteAttribute>(true);
+
+				Managers.Add(new EntityManagerInfo {Title = type.GetDisplayName(), Url = route.Template});
+			}
+
 			return this;
 		}
 	}
