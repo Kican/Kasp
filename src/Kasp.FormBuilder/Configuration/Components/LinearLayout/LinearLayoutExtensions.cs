80f47e59fae8f0290b19b517a72d26c520384784
mo3in <moein.hente@gmail.com>
2020-08-26 02:50:52 +0430

    idk ...

---
+++ src/Kasp.FormBuilder/Configuration/Components/LinearLayout/LinearLayoutExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Kasp.FormBuilder.Configuration.Components.LinearLayout {
+	public static class LinearLayoutExtensions {
+		public static ILinearLayoutConfiguration<TEntity> WithLinearLayout<TEntity>(this IConfigureBuilder<TEntity> builder, Action<ILinearLayoutConfiguration<TEntity>> builderAction) {
+			return null;
+		}
+	}
+}
---
+++ src/Kasp.FormBuilder/Configuration/IConfigureBuilder.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using Kasp.FormBuilder.Components;
+
+namespace Kasp.FormBuilder.Configuration {
+	public interface IConfigureBuilder<TEntity> {
+		void WithLayout<TLayout, TConfiguration>(Action<TConfiguration> configureAction) where TLayout : ILayoutComponent where TConfiguration : IComponentConfiguration<TLayout, TEntity>;
+	}
+}
