c45890e48f04f3c2d93a4f178d61f50fbcc9c863
mo3in <moein.hente@gmail.com>
2021-10-13 06:40:00 -0700

    update to .net 6

--- src/Kasp.Data/Specification/Specification.cs
+++ src/Kasp.Data/Specification/Specification.cs
@@ -5,6 +5,10 @@ namespace Kasp.Data.Specification {
 	public abstract class Specification<T> : ISpecification<T> {
 		public abstract Expression<Func<T, bool>> ToExpression();
 
+		public virtual bool IsSatisfiedBy(T obj) {
+			return ToExpression().Compile()(obj);
+		}
+
 		public static implicit operator Expression<Func<T, bool>>(Specification<T> specification) {
 			return specification.ToExpression();
 		}
